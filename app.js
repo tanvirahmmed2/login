ca8c44f18f227d660365eedb1e4858ed2a733aac
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-07-31 12:22:15 +0600

    update

--- app.js
+++ app.js
@@ -1,32 +1,66 @@
 
-const exprees= require("express")
-const cors =require("cors")
-const ejs= require('ejs')
-const app=exprees()
+const exprees = require("express")
+const cors = require("cors")
+const ejs = require('ejs')
+const app = exprees()
+const bcrypt = require("bcrypt")
+const saltRounds = 10
 
+const passport = require("passport")
+const session= require("express-session")
+
+require("./config")
+
+const User = require("./model/user.model")
 
 app.set("view engine", "ejs")
 app.use(cors())
-app.use(exprees.urlencoded({extended: true}))
+app.use(exprees.urlencoded({ extended: true }))
 app.use(exprees.json())
+
+app.set('trust proxy', 1) // trust first proxy
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+//   cookie: { secure: true }
+}))
 
 
 //home route
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.render("index")
 })
 
 //user register : get then post
 
-app.get("/register", (req,res)=>{
+app.get("/register", (req, res) => {
     res.render("register")
 })
 
 
 
-app.post("/register", (req,res)=>{
+app.post("/register", async (req, res) => {
+
     try {
-        res.status(200).send("user is created")
+        const user = await User.findOne({ username: req.body.username })
+        if (user) return res.status(400).send("user already exists")
+
+        bcrypt.hash(req.body.password, saltRounds, async (err, hash)=> {
+            const newUser = new User ({
+                username: req.body.username,
+                password: hash,
+            })
+
+
+            await newUser.save()
+
+
+            res.status(200).redirect("/login")
+        });
+
+
+
     } catch (error) {
         res.status(500).send(error.message)
     }
@@ -34,15 +68,19 @@ app.post("/register", (req,res)=>{
 
 
 //login page
-app.get("/login",(req,res)=>{
+app.get("/login", (req, res) => {
     res.render("login")
+})
+
+//login post
+app.post('/login', (req,res)=>{
+
 })
 
 
 
-
 //profile 
-app.get("/profile", (req,res)=>{
+app.get("/profile", (req, res) => {
     res.render("profile")
 })
 
@@ -52,7 +90,7 @@ app.get("/profile", (req,res)=>{
 
 
 //logout
-app.get("/logout", (req,res)=>{
+app.get("/logout", (req, res) => {
     res.redirect("/")
 })
 
@@ -79,4 +117,4 @@ app.get("/logout", (req,res)=>{
 
 
 
-module.exports= app
+module.exports = app
