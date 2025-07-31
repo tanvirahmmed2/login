c30ff368d8ba6c5f352349d9a249d3e1233094ab
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-07-31 12:42:44 +0600

    update

--- config/passport.js
+++ config/passport.js
@@ -12,7 +12,9 @@ passport.use(new LocalStrategy(
         if(!user){
             return done(null, false, {message: "incorrect username"})
         }
-        if(!bcrypt.compare(password, ))
+        if(!bcrypt.compare(password, user.password )){
+            return done(null, false, {message: "incorrect password"})
+        }
 
         return done(null, user);
     } catch (error) {
@@ -24,3 +26,17 @@ passport.use(new LocalStrategy(
    
   }
 ));
+
+//create session id
+ passport.serializeUser((user,done)=>{
+    done(null, user.id)
+ })
+
+ passport.deserializeUser(async (id, done)=>{
+    try {
+        const user= await User.findById(id)
+        done(null, user)
+    } catch (error) {
+        done(error, false)
+    }
+ })
